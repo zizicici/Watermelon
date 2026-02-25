371edb905cdd62a9a661e62bd4d935f219765961
zizicici <1940699+zizicici@users.noreply.github.com>
2026-02-25 12:12:59 +0800

    [feat] Inital

---
+++ PhotoBackup/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoBackup
+//
+//  Created by Ci Zi on 2026/2/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
