923684dd6b84df8a7197fedb268fc77abf0beec9
zizicici <1940699+zizicici@users.noreply.github.com>
2026-04-22 16:02:07 +0800

    [chore] Bump Version: 1.0.0 -> 1.0.1

--- Watermelon/Home/HomeViewController.swift
+++ Watermelon/Home/HomeViewController.swift
@@ -886,7 +886,7 @@ final class HomeViewController: UIViewController {
                     .init(type: .version, value: SpecificationsViewController.getAppVersion() ?? ""),
                     .init(type: .manufacturer, value: "@App君"),
                     .init(type: .publisher, value: "ZIZICICI LIMITED"),
-                    .init(type: .dateOfProduction, value: "2026/04/17"),
+                    .init(type: .dateOfProduction, value: "2026/04/22"),
                     .init(type: .license, value: "粤ICP备2025448771号-6A"),
                 ],
                 thirdPartyLibraries: [
