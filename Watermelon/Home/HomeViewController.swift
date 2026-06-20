6d9c68a96d4d5a5b7356941f91e15ee199c9d65e
zizicici <1940699+zizicici@users.noreply.github.com>
2026-06-20 10:29:43 +0800

    [chore] Bump Version: 1.4.4 -> 1.5.0

--- Watermelon/Home/HomeViewController.swift
+++ Watermelon/Home/HomeViewController.swift
@@ -831,11 +831,11 @@ final class HomeViewController: UIViewController {
                     .init(type: .version, value: SpecificationsViewController.getAppVersion() ?? ""),
                     .init(type: .manufacturer, value: "@App君"),
                     .init(type: .publisher, value: "ZIZICICI LIMITED"),
-                    .init(type: .dateOfProduction, value: "2026/06/13"),
+                    .init(type: .dateOfProduction, value: "2026/06/20"),
                     .init(type: .license, value: "粤ICP备2025448771号-6A"),
                 ],
                 thirdPartyLibraries: [
-                    .init(name: "AMSMB2", version: "4.0.3+f303dd8", urlString: "https://github.com/zizicici/AMSMB2"),
+                    .init(name: "AMSMB2", version: "main", urlString: "https://github.com/zizicici/AMSMB2"),
                     .init(name: "GRDB", version: "7.10.0", urlString: "https://github.com/groue/GRDB.swift"),
                     .init(name: "Kingfisher", version: "8.7.0", urlString: "https://github.com/onevcat/Kingfisher"),
                     .init(name: "MarqueeLabel", version: "4.5.3", urlString: "https://github.com/cbpowell/MarqueeLabel"),
